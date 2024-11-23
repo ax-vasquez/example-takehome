aeec5f8c565b62f44ffac665a59239782cf6fb5c
Mando <mando@mcnuggies.dev>
2024-11-22 19:56:34 -0600

    page title update

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Workflow Buttons</h1>
+        <h1>Workflow Testing</h1>
         <p className={styles.descriptionText}>Each option is labeled for the "entry_point" that the corresponding workflow starts with.</p>
         <div style={{
           display: "flex",
